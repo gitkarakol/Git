8022671c54735aab2a6477f71cc4b30deca79dd7
gitkarakol <155504733+gitkarakol@users.noreply.github.com>
2024-01-04 22:14:41 -0500

    Update HelloWorld.java
    CHANGED

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("heloooooo yallllll");
+        System.out.println("heloooooo usaaaa");
         System.out.println("my second change");
 
 
